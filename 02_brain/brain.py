7d09a76c29e478a87044f12d64f19baf673a8a5c
Brian Foo <brian@youaremyjoy.org>
2015-02-09 15:27:56 -0500

    added mean/stdev/min/max calculations

--- 02_brain/brain.py
+++ 02_brain/brain.py
@@ -10,18 +10,17 @@ import csv
 import json
 import math
 import os
-import random
-import re
 import time
 
 # Config
 BPM = 75 # Beats per minute, e.g. 60, 75, 100, 120, 150
 DIVISIONS_PER_BEAT = 16 # e.g. 4 = quarter notes, 8 = eighth notes, etc
 VARIANCE_MS = 10 # +/- milliseconds an instrument note should be off by to give it a little more "natural" feel
-PRECISION = 6
+PRECISION = 6 # decimal places after 0 for reading value
 INSTRUMENTS_INPUT_FILE = 'data/instruments.csv'
 EEG_INPUT_FILE = 'data/eeg.csv'
 REPORT_SUMMARY_OUTPUT_FILE = 'data/report_summary.csv'
+REPORT_SUMMARY_CHANNEL_OUTPUT_FILE = 'data/report_channel_summary.csv'
 REPORT_SEQUENCE_OUTPUT_FILE = 'data/report_sequence.csv'
 INSTRUMENTS_OUTPUT_FILE = 'data/ck_instruments.csv'
 SEQUENCE_OUTPUT_FILE = 'data/ck_sequence.csv'
@@ -29,12 +28,14 @@ VISUALIZATION_OUTPUT_FILE = 'visualization/data/eeg.json'
 INSTRUMENTS_DIR = 'instruments/'
 WRITE_SEQUENCE = False
 WRITE_REPORT = False
-WRITE_JSON = True
+WRITE_JSON = False
 LABELS = ['Time', 'FP1-F7', 'F7-T7', 'T7-P7', 'P7-O1', 'FP1-F3', 'F3-C3', 'C3-P3', 'P3-O1', 'FP2-F4', 'F4-C4', 'C4-P4', 'P4-O2', 'FP2-F8', 'F8-T8', 'T8-P8', 'P8-O2', 'FZ-CZ', 'CZ-PZ', 'P7-T7', 'T7-FT9', 'FT9-FT10', 'FT10-T8', 'T8-P8']
 
 # Calculations
 BEAT_MS = round(60.0 / BPM * 1000)
+MEASURE_MS = BEAT_MS * 4.0
 ROUND_TO_NEAREST = round(BEAT_MS/DIVISIONS_PER_BEAT)
+CHANNEL_COUNT = len(LABELS) - 1
 
 print('Building sequence at '+str(BPM)+' BPM ('+str(BEAT_MS)+'ms per beat)')
 
@@ -43,6 +44,7 @@ instruments = []
 eeg = []
 eeg_min = []
 eeg_max = []
+measures = []
 abs_min = 0
 abs_max = 0
 sequence = []
@@ -55,10 +57,38 @@ def halton(index, base):
 	f = 1.0 / base
 	i = 1.0 * index
 	while(i > 0):
-		result += f * (i % base)  
+		result += f * (i % base)	
 		i = math.floor(i / base)
 		f = f / base
 	return result
+
+# Mean of list
+def mean(data):
+    if iter(data) is data:
+			data = list(data)
+    n = len(data)
+    if n < 1:
+			return 0
+    else:
+			return sum(data)/n
+
+# Variance of list
+def variance(data):
+    if iter(data) is data:
+			data = list(data)
+    n = len(data)
+    if n < 1:
+			return 0
+    else:
+			c = mean(data)
+			ss = sum((x-c)**2 for x in data)
+			ss -= sum((x-c) for x in data)**2/len(data)
+			return ss/n
+
+# Standard deviation of list
+def stdev(data):
+    var = variance(data)
+    return math.sqrt(var)
 
 # Find index of first item that matches value
 def findInList(list, key, value):
@@ -76,14 +106,15 @@ def roundToNearest(n, nearest):
 with open(INSTRUMENTS_INPUT_FILE, 'rb') as f:
 	r = csv.reader(f, delimiter='\t')
 	next(r, None) # remove header
-	for name,type,weight,file,from_gain,to_gain,from_tempo,to_tempo,gain_phase,tempo_phase,tempo_offset,interval_phase,interval,interval_offset,active in r:
+	for name,phases,channel,weight,file,from_gain,to_gain,from_tempo,to_tempo,gain_phase,tempo_phase,tempo_offset,interval_phase,interval,interval_offset,active in r:
 		if file and int(active):
 			index = len(instruments)
 			# build instrument object
 			instrument = {
 				'index': index,
 				'name': name,
-				'type': type.lower().replace(' ', '_'),
+				'phases': phases.split(','),
+				'channel': int(channel),
 				'weight': float(weight),
 				'file': INSTRUMENTS_DIR + file,
 				'from_gain': round(float(from_gain), 2),
@@ -114,11 +145,40 @@ def normalizeRow(row, min_value, max_value):
 			normalized_row.append(col)
 	return normalized_row
 
-# Read stations from file
+def getWavelength(data, _min, _max, _stdev):
+	waves = []
+	start = -1
+	bottom_found = False
+	end = -1
+	data_len = len(data)
+	for index, value in enumerate(data):
+		# Look for the first peak
+		if value > (_max-_stdev) and start < 0:
+			start = index
+		# Look for the bottom crest
+		elif value < (_min+_stdev) and start >= 0 and not bottom_found and end < 0:
+			bottom_found = True
+		# Look for second peak
+		elif bottom_found and value > (_max-_stdev) and end < 0:
+			end = index
+			# Add wave to list and reset
+			waves.append(1.0 * (end - start) / data_len)
+			start = -1
+			bottom_found = False
+			end = -1
+	# wave(s) found, return average length
+	if len(waves) > 0:
+		return mean(waves)
+	else:
+		return 0
+
+# Read eeg from file
 with open(EEG_INPUT_FILE, 'rb') as f:
 	r = csv.reader(f, delimiter=',')
 	next(r, None) # remove header
 	last_ms = 0
+	measure = []
+	next_measure = MEASURE_MS
 	for t,s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16,s17,s18,s19,s20,s21,s22,s23 in r:
 		ms = int(t)
 		row = [ms,float(s1),float(s2),float(s3),float(s4),float(s5),float(s6),float(s7),float(s8),float(s9),float(s10),float(s11),float(s12),float(s13),float(s14),float(s15),float(s16),float(s17),float(s18),float(s19),float(s20),float(s21),float(s22),float(s23)]
@@ -134,8 +194,103 @@ with open(EEG_INPUT_FILE, 'rb') as f:
 		else:
 			normalized_row = normalizeRow(row, abs_min, abs_max)
 			eeg.append(normalized_row)
+			if ms >= next_measure:
+				measures.append({								
+					"readings": measure,
+					"channels": [],
+					"duration": MEASURE_MS
+				})
+				measure = []
+				next_measure += MEASURE_MS
+			else:
+				measure.append(normalized_row[1:])
 			total_ms += (ms - last_ms)
 			last_ms = ms
+	# Add the last measure
+	if len(measure) > 0:
+		measures.append({				
+			"readings": measure,
+			"channels": [],
+			"duration": total_ms - MEASURE_MS * len(measures)
+		})
+
+# Keep track of min/max stdev for normalization
+min_stdev = 0
+max_stdev = 0
+min_mean_stdev = 0
+max_mean_stdev = 0
+min_stdev2 = 0
+max_stdev2 = 0
+min_wavelength = 0
+max_wavelength = 0
+			
+# Go through each measure
+for mindex, measure in enumerate(measures):
+	channels = []
+	stdevs = []
+	maxs = []
+	wavelengths = []
+	# Create an array of channel-value arrays
+	for channel in range(CHANNEL_COUNT):
+		channels.append([])
+	for reading in measure["readings"]:
+		for channel, value in enumerate(reading):
+			channels[channel].append(value)
+	# For each channel
+	for cindex, channel in enumerate(channels):
+		# Calculate stdev, min/max, wavelength
+		_stdev = stdev(channel)
+		_min = min(channel)
+		_max = max(channel)
+		_wavelength = getWavelength(channel, _min, _max, _stdev)
+		stdevs.append(_stdev)
+		maxs.append(_max)
+		if _wavelength > 0:
+			wavelengths.append(_wavelength)
+			# Keep track of max/mins
+			if _wavelength > max_wavelength:
+				max_wavelength = _wavelength
+			if _wavelength < min_wavelength or min_wavelength==0:
+				min_wavelength = _wavelength
+		if _stdev > max_stdev:
+			max_stdev = _stdev
+		if _stdev < min_stdev or min_stdev==0:
+			min_stdev = _stdev
+		# Add to channel list to measure
+		measures[mindex]["channels"].append({
+			"stdev": _stdev,
+			"max": _max,
+			"wavelength": _wavelength
+		})
+	# Calculate max/means/stdevs
+	mean_stdev = mean(stdevs)
+	stdev_stdev = stdev(stdevs)
+	measures[mindex]["max"] = max(maxs)
+	measures[mindex]["mean_stdev"] = mean_stdev
+	measures[mindex]["mean_wavelength"] = mean(wavelengths)
+	measures[mindex]["stdev_stdev"] = stdev_stdev
+	# Keep track of min/max
+	if mean_stdev > max_mean_stdev:
+		max_mean_stdev = mean_stdev
+	if mean_stdev < min_mean_stdev or min_mean_stdev==0:
+		min_mean_stdev = mean_stdev
+	if stdev_stdev > max_stdev2:
+		max_stdev2 = stdev_stdev
+	if stdev_stdev < min_stdev2 or min_stdev2==0:
+		min_stdev2 = stdev_stdev
+
+# Normalize all values in measures
+for mindex, measure in enumerate(measures):
+	stdev_delta = max_stdev - min_stdev
+	stdev_mean_delta = max_mean_stdev - min_mean_stdev
+	stdev2_delta = max_stdev2 - min_stdev2
+	wavelength_delta = max_wavelength - min_wavelength
+	measures[mindex]["mean_stdev"] = 1.0 * (measure["mean_stdev"]-min_mean_stdev) / stdev_mean_delta
+	measures[mindex]["stdev_stdev"] = 1.0 * (measure["stdev_stdev"]-min_stdev2) / stdev2_delta
+	measures[mindex]["mean_wavelength"] = 1.0 * (measure["mean_wavelength"]-min_wavelength) / wavelength_delta
+	for cindex, channel in enumerate(measure["channels"]):
+		measures[mindex]["channels"][cindex]["stdev"] = 1.0 * (channel["stdev"]-min_stdev) / stdev_delta
+		measures[mindex]["channels"][cindex]["wavelength"] = max(1.0 * (channel["wavelength"]-min_wavelength) / wavelength_delta, 0)
 
 # Calculate total time
 total_seconds = int(1.0*total_ms/1000)
@@ -151,7 +306,7 @@ for index, step in enumerate(sequence):
 	elapsed = step['elapsed_ms']
 
 # Write instruments to file
-if WRITE_SEQUENCE:
+if WRITE_SEQUENCE and len(instruments) > 0:
 	with open(INSTRUMENTS_OUTPUT_FILE, 'wb') as f:
 		w = csv.writer(f)	
 		for index, instrument in enumerate(instruments):
@@ -162,7 +317,7 @@ if WRITE_SEQUENCE:
 		print('Successfully wrote instruments to file: '+INSTRUMENTS_OUTPUT_FILE)
 
 # Write sequence to file
-if WRITE_SEQUENCE:
+if WRITE_SEQUENCE and len(sequence) > 0:
 	with open(SEQUENCE_OUTPUT_FILE, 'wb') as f:
 		w = csv.writer(f)	
 		for step in sequence:
@@ -175,16 +330,34 @@ if WRITE_SEQUENCE:
 		f.truncate()
 		print('Successfully wrote sequence to file: '+SEQUENCE_OUTPUT_FILE)
 
-# Write summary file
+# Write summary files
 if WRITE_REPORT:
 	with open(REPORT_SUMMARY_OUTPUT_FILE, 'wb') as f:
 		w = csv.writer(f)
-		w.writerow(['Time', 'Name', 'Duration', 'Beats', 'Instruments'])
-		elapsed = 0		
+		w.writerow(['Time', 'Mean-Stdev', 'Stdev-Stdev', 'Mean-Wavelength', 'Duration'])
+		for mindex, measure in enumerate(measures):
+			elapsed = mindex * MEASURE_MS
+			elapsed_f = time.strftime('%M:%S', time.gmtime(int(elapsed/1000)))
+			ms = int(elapsed % 1000)
+			elapsed_f += '.' + str(ms)
+			w.writerow([elapsed_f, measure['mean_stdev'], measure['stdev_stdev'], measure['mean_wavelength'], int(measure['duration'])])
 		print('Successfully wrote summary file: '+REPORT_SUMMARY_OUTPUT_FILE)
+	with open(REPORT_SUMMARY_CHANNEL_OUTPUT_FILE, 'wb') as f:
+		w = csv.writer(f)
+		w.writerow(LABELS)
+		for mindex, measure in enumerate(measures):
+			elapsed = mindex * MEASURE_MS
+			elapsed_f = time.strftime('%M:%S', time.gmtime(int(elapsed/1000)))
+			ms = int(elapsed % 1000)
+			elapsed_f += '.' + str(ms)
+			channels = [elapsed_f]
+			for channel in measure["channels"]:
+				channels.append(channel["stdev"])
+			w.writerow(channels)
+		print('Successfully wrote channel summary file: '+REPORT_SUMMARY_CHANNEL_OUTPUT_FILE)
 
 # Write sequence report to file
-if WRITE_REPORT:
+if WRITE_REPORT and len(sequence) > 0:
 	with open(REPORT_SEQUENCE_OUTPUT_FILE, 'wb') as f:
 		w = csv.writer(f)
 		w.writerow(['Time', 'Instrument', 'Gain'])
@@ -192,7 +365,7 @@ if WRITE_REPORT:
 			instrument = instruments[step['instrument_index']]
 			elapsed = step['elapsed_ms']
 			elapsed_f = time.strftime('%M:%S', time.gmtime(int(elapsed/1000)))
-			ms = elapsed % 1000
+			ms = int(elapsed % 1000)
 			elapsed_f += '.' + str(ms)
 			w.writerow([elapsed_f, instrument['file'], step['gain']])
 		f.seek(-2, os.SEEK_END) # remove newline
