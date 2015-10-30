e785267c05ee27020fd14673ae2635098c5b3387
beefoo <brian@youaremyjoy.org>
2015-10-30 11:08:35 -0400

    body: implement query lyrics script

--- 08_body/query_lyrics.py
+++ 08_body/query_lyrics.py
@@ -28,12 +28,45 @@ with open(WORDS_FILE, 'rb') as f:
 # Read songs/lyrics
 with open(LYRICS_FILE) as f:
     songs = json.load(f)
+
 # Filter songs for this artist
 songs = [s for s in songs if artist.lower() in s['artist'].lower()]
 if len(songs) <= 0:
     print "No matches for artist: " + artist
     sys.exit(1)
 
+def addSongMatch(song):
+    global song_matches
+
+    matches = [s for s in song_matches if s['url']==song['url']]
+    # if already exists, just increment score
+    if len(matches) > 0:
+        match = matches[0]
+        song_matches[match['index']]['score'] += 1
+    # add if not already exist
+    else:
+        song['score'] = 1
+        song['index'] = len(song_matches)
+        song_matches.append(song)
+
+print "Analyzing..."
+
 # Analyze each song
 for song in songs:
     lWords = song['lyrics'].split(' ')
+    for lw in lWords:
+        matches = [w for w in words if w['word']==lw and w['region']==body_part]
+        if len(matches) > 0:
+            addSongMatch(song)
+
+# Sort song matches by score
+song_matches = sorted(song_matches, key=lambda k: k['score'], reverse=True)
+
+# Report on matches
+if len(song_matches) > 0:
+    print str(len(song_matches)) + ' matches found: '
+else:
+    print 'No results found'
+
+for s in song_matches:
+    print str(s['score']) + '. ' + s['song'] + ' - ' + s['album'] + ' [' + s['url'] + ']'
